13e63c5fe03f945a0849284dc1eb8ac991eefc69
Vijay Gehlot <vijaygehlot208@gmail.com>
2021-08-02 09:37:40 +0530

    Update firebase.js

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -4,15 +4,7 @@ import "firebase/firestore"
 import "firebase/storage"
 
 const firebaseConfig = {
-    // apiKey: "AIzaSyB7zTYmhFI7Ygvhf_f1VTHTWAAGN9VlJoI",
-    // authDomain: "forms-clone-127b9.firebaseapp.com",
-    // projectId: "forms-clone-127b9",
-    // storageBucket: "forms-clone-127b9.appspot.com",
-    // messagingSenderId: "356296216795",
-    // appId: "1:356296216795:web:df779d62a4322f75a43cfe"
-
-
-    apiKey: "AIzaSyCh67YBJsPcwQogpjlytSkl7zclblxeb6I",
+apiKey: "AIzaSyCh67YBJsPcwQogpjlytSkl7zclblxeb6I",
 authDomain: "getforms-5f406.firebaseapp.com",
 projectId: "getforms-5f406",
 storageBucket: "getforms-5f406.appspot.com",
